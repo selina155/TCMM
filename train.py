d547bc5ce4a0dc8b17fd7f4f1de0e3f6d7a7b579
selina155 <164332634+selina155@users.noreply.github.com>
2025-08-07 21:26:14 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -20,9 +20,9 @@ from src.utils.metrics_utils import evaluate_results
 from src.model.generate_model import generate_model
 import warnings
 
-
 warnings.filterwarnings('ignore')
 np.set_printoptions(threshold=np.inf)
+
 
 @hydra.main(version_base=None, config_path="../configs/", config_name="main.yaml")
 def run(cfg: DictConfig):
@@ -48,7 +48,7 @@ def train(cfg):
     dataset = cfg.dataset
     seed = int(cfg.random_seed)
     # set random seed
-    seed_everything(cfg.random_seed,workers=True)
+    seed_everything(cfg.random_seed, workers=True)
     X, adj_matrix, aggregated_graph, lag, data_dim = load_data(
         cfg.dataset, cfg.dataset_dir, cfg)
 
@@ -57,10 +57,10 @@ def train(cfg):
     add_attribute(cfg, 'num_nodes', X.shape[2])
     add_attribute(cfg, 'data_dim', data_dim)
 
-    #model
+    # model
     model = generate_model(cfg)
 
-    #dataset
+    # dataset
     model = model(full_dataset=X,
                   adj_matrices=adj_matrix)
     model_name = cfg.model
@@ -71,7 +71,7 @@ def train(cfg):
         mix_idx = torch.Tensor(np.load(f_path))
         model.set_mixture_indices(mix_idx)
 
-    training_needed = cfg.model in ['rhino', 'mcd','gnn']
+    training_needed = cfg.model in ['rhino', 'mcd', 'tcmm']
     unique_name = generate_unique_name(cfg)
     csv_logger = CSVLogger("logs", name=unique_name)
 
@@ -85,10 +85,10 @@ def train(cfg):
         checkpoint_callback = ModelCheckpoint(
             save_top_k=1, monitor=monitor_checkpoint_based_on, mode="min", save_last=True)
 
-        if len(cfg.gpu) > 1:
-            strategy = 'ddp_find_unused_parameters_true'
-        else:
-            strategy = 'auto'
+        # if len(cfg.gpu) > 1:
+        #     strategy = 'ddp_find_unused_parameters_true'
+        # else:
+        #     strategy = 'auto'
 
         if 'val_every_n_epochs' in cfg:
             val_every_n_epochs = cfg.val_every_n_epochs
@@ -96,12 +96,12 @@ def train(cfg):
             val_every_n_epochs = 1
 
         trainer = Trainer(max_epochs=cfg.num_epochs,
-                          accelerator="gpu",
-                          devices=cfg.gpu,
+                          accelerator="cpu",
+                          devices=cfg.cpu,
                           precision=cfg.precision,
                           logger=csv_logger,
                           callbacks=[checkpoint_callback],
-                          strategy=strategy,
+                          #strategy=strategy,
                           enable_progress_bar=True,
                           check_val_every_n_epoch=val_every_n_epochs,
                           )
@@ -113,10 +113,10 @@ def train(cfg):
         if checkpoint_path is None:
             trainer.fit(model=model)
         else:
-            trainer.fit(model=model,ckpt_path=checkpoint_path)
+            trainer.fit(model=model, ckpt_path=checkpoint_path)
         end_time = time.time()
 
-        print("Model trained in", str(end_time-start_time) + "s")
+        print("Model trained in", str(end_time - start_time) + "s")
     else:
         if cfg.gpu != -1:
             print("WARNING: GPU specified, but baseline cannot use GPU.")
@@ -141,12 +141,13 @@ def train(cfg):
     predictions = torch.concatenate(predictions, dim=0)
     scores = torch.concatenate(scores, dim=0)
     adj_matrix = torch.concatenate(adj_matrix, dim=0)
-    
+
     predictions = predictions.detach().cpu().numpy()
     scores = scores.detach().cpu().numpy()
     adj_matrix = adj_matrix.detach().cpu().numpy()
 
-    if dataset in ['ecoli1','ecoli2','yeast1','yeast2','yeast3','dream3_combined']:#ignore self-connections in dream3 dataset
+    if dataset in ['ecoli1', 'ecoli2', 'yeast1', 'yeast2', 'yeast3',
+                   'dream3_combined']:  # ignore self-connections in dream3 dataset
         predictions = zero_out_diag_np(predictions)
         scores = zero_out_diag_np(scores)
 
@@ -155,15 +156,15 @@ def train(cfg):
         os.makedirs(os.path.join('results', dataset))
 
     if training_needed and ckpt_choice == 'best':
-        if model_name in ('mcd','gnn'):
+        if model_name in ('mcd', 'tcmm'):
             np.save(os.path.join('results', dataset,
-                    f'{model_name}_{checkpoint_callback.best_model_score.item()}_k{cfg.trainer.num_graphs}.npy'), scores)
+                                 f'{model_name}_{checkpoint_callback.best_model_score.item()}_k{cfg.trainer.num_graphs}.npy'),
+                    scores)
         else:
             np.save(os.path.join('results', dataset,
-                    f'{model_name}_{checkpoint_callback.best_model_score.item()}.npy'), scores)
+                                 f'{model_name}_{checkpoint_callback.best_model_score.item()}.npy'), scores)
     else:
         np.save(os.path.join('results', dataset, f'{model_name}.npy'), scores)
-
 
     metrics = evaluate_results(scores=scores,
                                adj_matrix=adj_matrix,
@@ -180,7 +181,6 @@ def train(cfg):
     # write the results to the log directory
     write_results_to_disk(create_save_name(dataset, cfg), metrics)
 
+
 if __name__ == "__main__":
     run()
-
-
